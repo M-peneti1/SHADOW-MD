ceb4bf7db7f61fecc22c4ea5059899d12a16ca0d
M-peneti1 <blotgiri21@gmail.com>
2024-10-09 20:34:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
-SUDO_NB: process.env.SUDO_NB || "94767910958",
+SESSION_ID: process.env.SESSION_ID || "wvc3HZCY#07iEDSuaBNUMKFgM-vTEC1TBLmHxjC7hEZy5mLmDTRM",
+SUDO_NB: process.env.SUDO_NB || "263789669160",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_BIO: process.env.AUTO_BIO || "false",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "unavailable",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "false",
 AUTO_TYPING:"true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"true",
+AUTO_VOICE:"false",
 };
